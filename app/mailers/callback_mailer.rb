14ee31700ac788e77e223a550bef181002f54ee6
Arif Alim <arifalim9@gmail.com>
2018-09-11 23:33:09 -0400

    added mo and ali emails and cc'd me

--- app/mailers/callback_mailer.rb
+++ app/mailers/callback_mailer.rb
@@ -1,9 +1,10 @@
 class CallbackMailer < ApplicationMailer
   default from: 'bestinsulation2018@gmail.com'
+  TO = ["ali@bestinsulation.ca", "mo@bestinsulation.ca"]
 
   # send a signup email to the user, pass in the user object that   contains the user's email address
   def callback_request(callback)
     @callback = callback
-    mail( to: "ali@bestinsulation.ca", subject: "Callback Request from #{@callback.name.camelcase} #{Time.current.strftime('%b %d, %Y %H:%M %p')}", cc: "bestinsulation2018@gmail.com" )
+    mail( to: TO, subject: "Callback Request from #{@callback.name.camelcase} #{Time.current.strftime('%b %d, %Y %H:%M %p')}", cc: "arifalim9@gmail.com" )
   end
 end
