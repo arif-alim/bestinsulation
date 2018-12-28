419c846625e8c2ddc16c3dd05f028ebbd18321d2
Arif Alim <arifalim@Arifs-Macbook-Pro.local>
2018-12-28 18:18:32 -0500

    removed ali's information

--- app/mailers/quote_mailer.rb
+++ app/mailers/quote_mailer.rb
@@ -1,10 +1,10 @@
 class QuoteMailer < ApplicationMailer
   default from: 'bestinsulation2018@gmail.com'
-  TO = ["ali@bestinsulation.ca", "mo@bestinsulation.ca"]
+  # TO = ["ali@bestinsulation.ca", "mo@bestinsulation.ca"]
 
   # send a signup email to the user, pass in the user object that   contains the user's email address
   def quote_request(quote)
     @quote = quote
-    mail( to: TO, subject: "Quote Request by #{@quote.name.camelcase} #{Time.current.strftime('%b %d, %Y %H:%M %p')}", cc: "arifalim9@gmail.com" )
+    mail( to: "mo@bestinsulation.ca", subject: "Quote Request by #{@quote.name.camelcase} #{Time.current.strftime('%b %d, %Y %H:%M %p')}", cc: "arifalim9@gmail.com" )
   end
 end
