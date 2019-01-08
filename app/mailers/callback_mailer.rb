3709a5bc0a726b8ce75e86ddf86889a97ed4106d
Arif Alim <arifalim@Arifs-Macbook-Pro.local>
2019-01-08 18:26:56 -0500

    fixed the mailer

--- app/mailers/callback_mailer.rb
+++ app/mailers/callback_mailer.rb
@@ -1,10 +1,11 @@
 class CallbackMailer < ApplicationMailer
   default from: 'bestinsulation2018@gmail.com'
   # TO = ["ali@bestinsulation.ca", "mo@bestinsulation.ca"]
+  TO = "mo@bestinsulation.ca"
 
   # send a signup email to the user, pass in the user object that   contains the user's email address
   def callback_request(callback)
     @callback = callback
-    mail( to: "mo@bestinsulation.ca", subject: "Callback Request from #{@callback.name.camelcase} #{Time.current.strftime('%b %d, %Y %H:%M %p')}", cc: "arifalim9@gmail.com" )
+    mail( to: TO, subject: "Callback Request from #{@callback.name.camelcase} #{Time.current.strftime('%b %d, %Y %H:%M %p')}", cc: "arifalim9@gmail.com" )
   end
 end
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,10 +1,11 @@
 class ContactMailer < ApplicationMailer
   default from: 'bestinsulation2018@gmail.com'
   # TO = ["ali@bestinsulation.ca", "mo@bestinsulation.ca"]
+  TO = "mo@bestinsulation.ca"
 
   # send a signup email to the user, pass in the user object that   contains the user's email address
   def contact_us(contact)
     @contact = contact
-    mail( to: "mo@bestinsulation.ca", subject: "Contact Us Request by #{@contact.name.camelcase} #{Time.current.strftime('%b %d, %Y %H:%M %p')}", cc: "arifalim9@gmail.com" )
+    mail( to: TO, subject: "Contact Us Request by #{@contact.name.camelcase} #{Time.current.strftime('%b %d, %Y %H:%M %p')}", cc: "arifalim9@gmail.com" )
   end
 end
