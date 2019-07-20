8923fa8326915f35848dc66738e9098af482903e
Arif Alim <arifalim9@gmail.com>
2019-07-19 21:28:47 -0400

    removed arif's email address

--- app/mailers/callback_mailer.rb
+++ app/mailers/callback_mailer.rb
@@ -6,6 +6,6 @@ class CallbackMailer < ApplicationMailer
   # send a signup email to the user, pass in the user object that   contains the user's email address
   def callback_request(callback)
     @callback = callback
-    mail( to: TO, subject: "Callback Request from #{@callback.name.camelcase} #{Time.current.strftime('%b %d, %Y %H:%M %p')}", cc: "arifalim9@gmail.com" )
+    mail( to: TO, subject: "Callback Request from #{@callback.name.camelcase} #{Time.current.strftime('%b %d, %Y %H:%M %p')}")
   end
 end
--- app/mailers/quote_mailer.rb
+++ app/mailers/quote_mailer.rb
@@ -6,6 +6,6 @@ class QuoteMailer < ApplicationMailer
   # send a signup email to the user, pass in the user object that   contains the user's email address
   def quote_request(quote)
     @quote = quote
-    mail( to: TO, subject: "Quote Request by #{@quote.name.camelcase} #{Time.current.strftime('%b %d, %Y %H:%M %p')}", cc: "arifalim9@gmail.com" )
+    mail( to: TO, subject: "Quote Request by #{@quote.name.camelcase} #{Time.current.strftime('%b %d, %Y %H:%M %p')}")
   end
 end
