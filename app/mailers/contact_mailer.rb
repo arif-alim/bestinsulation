dd16e624d61577daac8fc03ff73caf50376adb53
Arif Alim <arifalim9@gmail.com>
2019-07-19 21:24:19 -0400

    removed arif's email address

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,11 +1,10 @@
 class ContactMailer < ApplicationMailer
   default from: 'bestinsulation2018@gmail.com'
-  # TO = ["ali@bestinsulation.ca", "mo.shahri@yahoo.ca"]
   TO = ["mo.shahri@yahoo.ca", "r.bestinsulation@gmail.com"]
 
   # send a signup email to the user, pass in the user object that   contains the user's email address
   def contact_us(contact)
     @contact = contact
-    mail( to: TO, subject: "Contact Us Request by #{@contact.name.camelcase} #{Time.current.strftime('%b %d, %Y %H:%M %p')}", cc: "arifalim9@gmail.com" )
+    mail( to: TO, subject: "Contact Us Request by #{@contact.name.camelcase} #{Time.current.strftime('%b %d, %Y %H:%M %p')}")
   end
 end
